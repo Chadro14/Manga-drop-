79002cea6b695207916fc93151eec9461ab5448c
Altesse Royal <altessemwamboko@gmail.com>
2026-06-10 13:18:30 +0100

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
-import { ThemeProvider } from './context/ThemeContext';
-import BottomNav from './components/common/BottomNav';
+import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import CatalogPage from './pages/CatalogPage';
 import MangaPage from './pages/MangaPage';
@@ -12,27 +9,18 @@ import ProfilePage from './pages/ProfilePage';
 import SettingsPage from './pages/SettingsPage';
 import NotFoundPage from './pages/NotFoundPage';
 
-function App() {
+export default function App() {
   return (
-    <ThemeProvider>
-      <AuthProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/catalog" element={<CatalogPage />} />
-            <Route path="/manga/:id" element={<MangaPage />} />
-            <Route path="/read/:chapterId" element={<ReaderPage />} />
-            <Route path="/auth" element={<AuthPage />} />
-            <Route path="/dashboard" element={<DashboardPage />} />
-            <Route path="/profile/:userId" element={<ProfilePage />} />
-            <Route path="/settings" element={<SettingsPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-          <BottomNav />
-        </BrowserRouter>
-      </AuthProvider>
-    </ThemeProvider>
+    <Routes>
+      <Route path="/"                  element={<HomePage />} />
+      <Route path="/catalog"           element={<CatalogPage />} />
+      <Route path="/manga/:id"         element={<MangaPage />} />
+      <Route path="/read/:chapterId"   element={<ReaderPage />} />
+      <Route path="/auth"              element={<AuthPage />} />
+      <Route path="/dashboard"         element={<DashboardPage />} />
+      <Route path="/profile/:userId"   element={<ProfilePage />} />
+      <Route path="/settings"          element={<SettingsPage />} />
+      <Route path="*"                  element={<NotFoundPage />} />
+    </Routes>
   );
 }
-
-export default App;
