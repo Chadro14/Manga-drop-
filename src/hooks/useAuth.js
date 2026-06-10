ded132af3ebc55f30f085d2e38125957af1606b6
Altesse Royal <altessemwamboko@gmail.com>
2026-06-10 13:21:30 +0100

    Update useAuth.js

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,10 +1,11 @@
+// src/hooks/useAuth.js
 import { useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
 
-export const useAuth = () => {
+export function useAuth() {
   const context = useContext(AuthContext);
   if (!context) {
-    throw new Error('useAuth must be used within AuthProvider');
+    throw new Error('useAuth doit être utilisé dans un AuthProvider');
   }
   return context;
-};
+}
