98f3a3f01cebbdb9c183097add68447bdea53887
Altesse Royal <altessemwamboko@gmail.com>
2026-06-12 02:09:18 +0100

    Create src/main.jsx

---
+++ client/src/main.jsx
@@ -0,0 +1,18 @@
+// src/main.jsx
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import App from './App';
+import './styles/variables.css';
+import './styles/index.css';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
